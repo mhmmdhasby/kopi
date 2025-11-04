75b28016ec427b1d5ca707836c5a8d3f4eb763ac
Prabowow <mhmmdhasbyreal@gmail.com>
2025-11-04 11:16:43 +0700

    update main.js

--- main.js
+++ main.js
@@ -12,16 +12,20 @@ let diskon = Number(document.getElementById("diskon").value)
 let subtotal = harga * jumlah
 
 // langkah 2: menghitung diskon
+let potongan = subtotal*(diskon/100)
 
-
-// langkah 3: menghitung pajak
+// langkah 3: menghitung pajak misal 11%
+let pajak = subtotal *(11/100)
 
 
 // langkah 4: hitung total
-
+let total = subtotal - potongan + pajak
 
 // langkah 5: menampilkan hasil
 document.getElementById("hasil").innerHTML = `
 Subtotal: Rp ${subtotal} <br>
+diskon(${diskon}%):-Rp ${potongan}<br>
+pajak(11%) : +Rp ${pajak} <br>
+Total Bayar :Rp ${total}
 `
 }
